1e1bbb225d253fa8df01b27ce1d7a1a243f0749a
WilliamFilh0 <williamfilho8908@gmail.com>
2025-02-04 21:53:10 -0300

    fix: resolve MDX processing error by updating dependencies, modifying project configurations, reorganizing directories, and renaming files

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,29 +1,45 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-
 import { join, dirname } from "path";
+import remarkGfm from "remark-gfm";
 
 /**
- * This function is used to resolve the absolute path of a package.
- * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ * Esta função é usada para resolver o caminho absoluto de um pacote.
+ * É necessária em projetos que utilizam Yarn PnP ou estão configurados dentro de um monorepo.
  */
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
 const config: StorybookConfig = {
-  stories: [
-    "../src/stories/**/*.stories.mdx",
-    "../src/stories/**/*.stories.tsx",
-  ],
+  stories: ["../pages/**/*.mdx", "../src/stories/**/*.stories.tsx"],
 
   addons: [
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@chromatic-com/storybook"),
     getAbsolutePath("@storybook/addon-interactions"),
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        mdxPluginOptions: {
+          remarkPlugins: [remarkGfm],
+        },
+      },
+    },
+    getAbsolutePath("@storybook/addon-links"),
+    getAbsolutePath("@storybook/addon-mdx-gfm"),
   ],
+
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
     options: {},
   },
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript",
+  },
 };
+
 export default config;
--- packages/docs/vite.config.ts
+++ packages/docs/vite.config.ts
@@ -5,6 +5,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["@mdx-js/react"],
+  },
+  server: {
+    fs: {
+      allow: ["../"], // Permite acesso a arquivos acima do diretório atual
+    },
+  },
 });
 
 // https://vite.dev/config/
